0d7c222ab22911770df6338bae7c7b94731d6186
danielburke <daniel.burke397@gmail.com>
2014-09-12 23:48:58 -0500

    Update BuildConfig.java

--- BuildConfig.java
+++ BuildConfig.java
@@ -1,20 +1,48 @@
-package com.example.speedforvolume;
+package com.example.speedup;
+
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.content.Context;
+import android.location.*;
+
 
 
 public class MainActivity extends ActionBarActivity {
-
+	private boolean accuracy;
+	private float speed;
+	private double lat, lon,alt;
+	private Location loc;
+	private LocationManager locMan;
+	/**
+	 * still need to add buttons and menues. Next I need to make on click listen to change screen
+	 */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start=(Button) findViewById(R.id.button1);
+        locMan=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        loc=locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        lat=loc.getLatitude();//gets current lat
+        lon=loc.getLongitude();//gets current longitude
+        lat=loc.getAltitude();//gets current altitude
+        accuracy=loc.hasAccuracy();
+        speed=loc.getSpeed();
+        start.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View view){
+        		setContentView(R.layout.screen2);
+        	}
+        });
     }
-
-
+    public void Start(){
+//    	if(locMan.)
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
